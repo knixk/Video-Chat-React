4c098a008359edc1541f77f4a7f36a5f7999cdc4
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2023-07-22 22:26:45 +0530

    changes

--- socket.js
+++ socket.js
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "https://2pwrql.csb.app/";
+  process.env.NODE_ENV === "production" ? undefined : "http://localhost:4000";
 
 export const socket = io(URL, {
-  autoConnect: false
+  autoConnect: false,
 });
